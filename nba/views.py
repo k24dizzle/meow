0fd6b11d54c828ffe51e40aa0b4a81f5ed3522af
zhaok24@uw.edu <zhaok24@uw.edu>
2016-02-22 18:04:53 -0800

    added a streak thingy for each team

--- nba/views.py
+++ nba/views.py
@@ -17,6 +17,17 @@ def score(request, game_id):
 
 def team(request, team):
     games = nba.objects.filter(Q(home_Team=team) | Q(away_Team=team)).filter(date__lte=timezone.now()).order_by('-date')
+    test = calcRecord(games, team)
+    winStreak = calcStreak(games, team)
+    return render(request, 'nba/team.html', {'games': games, 'team': team, 'wins': test[0], 'losses': test[1], 'streak': winStreak}) 
+
+def maps(request, team):
+    return render(request, 'nba/map.html', {'team': team})
+
+
+#calculates a team's record based on "games" and the teams name (to help identify if
+# they won or lost)
+def calcRecord(games, team):
     win = 0;
     loss = 0;
     for game in games:
@@ -30,8 +41,31 @@ def team(request, team):
                 win += 1
             else:
                 loss += 1
-    return render(request, 'nba/team.html', {'games': games, 'team': team, 'wins': win, 'losses': loss}) 
+    return [win, loss]
 
-def maps(request, team):
-    return render(request, 'nba/map.html', {'team': team})
-# Create your views here.
+def calcStreak(games, team):
+    streak = 0;
+    for game in games:
+        if(game.home_Team == team):
+            if game.home_Score > game.away_Score:
+                if (streak < 0):
+                    return streak
+                else: 
+                    streak += 1
+            else:
+                if (streak > 0):
+                    return streak
+                else:
+                    streak -= 1
+        else:
+            if game.away_Score > game.home_Score:
+                if streak < 0:
+                    return streak
+                else:
+                    streak += 1
+            else:
+                if streak > 0:
+                    return streak
+                else:
+                    streak -= 1
+    return streak
