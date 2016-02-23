d44713491830c8d77c2944971525c674264e75fa
zhaok24@uw.edu <zhaok24@uw.edu>
2016-02-22 17:37:54 -0800

    added records on the team pages

--- nba/views.py
+++ nba/views.py
@@ -17,7 +17,20 @@ def score(request, game_id):
 
 def team(request, team):
     games = nba.objects.filter(Q(home_Team=team) | Q(away_Team=team)).filter(date__lte=timezone.now()).order_by('-date')
-    return render(request, 'nba/team.html', {'games': games, 'team': team}) 
+    win = 0;
+    loss = 0;
+    for game in games:
+        if (game.home_Team == team):
+            if game.home_Score > game.away_Score:
+                win += 1
+            else: 
+                loss += 1
+        else:
+            if game.away_Score > game.home_Score:
+                win += 1
+            else:
+                loss += 1
+    return render(request, 'nba/team.html', {'games': games, 'team': team, 'wins': win, 'losses': loss}) 
 
 def maps(request, team):
     return render(request, 'nba/map.html', {'team': team})
