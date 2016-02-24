1d7ba9985a717ec7c61628eea6d0e532ec2e7740
zhaok24@uw.edu <zhaok24@uw.edu>
2016-02-23 17:53:52 -0800

    making the team page look a bit prettier with css, next up add adv statistics somehow

--- nba/views.py
+++ nba/views.py
@@ -23,9 +23,9 @@ def team(request, team):
         raise Http404("Team does not exist: " + team)
     games2 = nba.objects.filter(Q(home_Team=team) | Q(away_Team=team)).filter(date__lte=timezone.now()).order_by('-date')
     test = calcRecord(games2, team)
-    winStreak = calcStreak(games2, team)
-
-    return render(request, 'nba/team.html', {'games': games2, 'team': team, 'wins': test[0], 'losses': test[1], 'streak': winStreak}) 
+    winStreak = formatStreak(calcStreak(games2, team))
+    eff = calcEff(games2, team)
+    return render(request, 'nba/team.html', {'games': games2, 'team': team, 'wins': test[0], 'losses': test[1], 'streak': winStreak, 'offEff': eff[0], 'defEff': eff[1]}) 
 
 def maps(request, team):
     return render(request, 'nba/map.html', {'team': team})
@@ -68,14 +68,30 @@ def calcStreak(games, team):
             if game.away_Score > game.home_Score:
                 if streak < 0:
                     return streak
-                else:
+                else: 
                     streak += 1
             else:
                 if streak > 0:
                     return streak
                 else:
                     streak -= 1
+    return streak
+
+def formatStreak(streak):
     if (streak < 0):
         return "Lost " + str(streak * -1)
     else:
         return "Won " + str(streak)
+
+def calcEff(games, team):
+    total = len(games)
+    offense = 0
+    defense = 0
+    for game in games:
+        if game.home_Team == team:
+            offense += game.home_Score
+            defense += game.away_Score
+        else:
+            offense += game.away_Score
+            defense += game.home_Score
+    return ['{0:.2f}'.format(offense/total), '{0:.2f}'.format(defense/total)]
